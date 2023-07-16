ccdc9cee2a8be101873db73bac23963816e6660c
ivan <iw.sitnikoff@yandex.ru>
2023-07-16 21:16:01 +0300

    created models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class ApiUser(AbstractUser):
+    ...
+
+
+class Fold(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    fold = models.ForeignKey(Fold, related_name='products', on_delete=models.CASCADE)
+
+
+class Take(models.Model):
+    product = models.ForeignKey(Product, related_name='taken', on_delete=models.CASCADE)
+    user = models.ForeignKey(ApiUser, related_name='taken', on_delete=models.CASCADE)
